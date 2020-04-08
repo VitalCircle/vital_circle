175a94a30ebbc8aa23a8c6c23469945624a7cb3f
Erik Murphy <erik@induro.io>
2020-04-07 21:45:46 -0400

    adds model and api for user agreements

--- lib/constants/log_zone.dart
+++ lib/constants/log_zone.dart
@@ -1,7 +1,8 @@
 class LogZone {
   static const String AUTH = 'AUTH';
+  static const String CHECKUP = 'CHECKUP';
+  static const String FIRESTORE = 'FIRESTORE';
   static const String GEO = 'GEO';
   static const String LOCAL_STORAGE = 'LOCAL_STORAGE';
   static const String ROUTE = 'ROUTE';
-  static const String CHECKUP = 'CHECKUP';
 }
