a85732cb39e8a64e57e3081d1b62da594919618b
Erik Murphy <erik@induro.io>
2020-04-08 20:27:14 -0400

    half-baked

--- lib/screens/onboarding/privacy-agreement.dart
+++ lib/screens/onboarding/privacy-agreement.dart
@@ -1,40 +1,15 @@
 import 'package:flutter/material.dart';
 
-class PrivacyAgreementScreen extends StatelessWidget {
-  const PrivacyAgreementScreen({@required this.onNext});
+import 'legal.dart';
+
+class PrivacyAgreementScreen extends LegalAgreementScreen {
+  const PrivacyAgreementScreen({@required this.onNext}) : super(title: 'Privacy Agreement', content: LOREM_IPSUM);
+
   final VoidCallback onNext;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Builder(builder: (BuildContext context) {
-        return Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const Text('Privacy Agreement'),
-              RaisedButton(
-                onPressed: _onAccept,
-                child: const Text('Accept'),
-              ),
-              FlatButton(
-                onPressed: _onSkip,
-                child: const Text('Skip'),
-              )
-            ],
-          ),
-        );
-      }),
-    );
-  }
-
-  void _onAccept() {
+  void onAccept() {
     // TODO: Persist
-    onNext();
-  }
-
-  void _onSkip() {
     onNext();
   }
 }
--- lib/screens/onboarding/tos-agreement.dart
+++ lib/screens/onboarding/tos-agreement.dart
@@ -1,40 +1,15 @@
 import 'package:flutter/material.dart';
 
-class TermsOfServiceAgreementScreen extends StatelessWidget {
-  const TermsOfServiceAgreementScreen({@required this.onNext});
+import 'legal.dart';
+
+class TermsOfServiceAgreementScreen extends LegalAgreementScreen {
+  const TermsOfServiceAgreementScreen({@required this.onNext}) : super(title: 'Terms of Service', content: LOREM_IPSUM);
+
   final VoidCallback onNext;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Builder(builder: (BuildContext context) {
-        return Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const Text('Terms of Service Agreement'),
-              RaisedButton(
-                onPressed: _onAccept,
-                child: const Text('Accept'),
-              ),
-              FlatButton(
-                onPressed: _onSkip,
-                child: const Text('Skip'),
-              )
-            ],
-          ),
-        );
-      }),
-    );
-  }
-
-  void _onAccept() {
+  void onAccept() {
     // TODO: Persist
-    onNext();
-  }
-
-  void _onSkip() {
     onNext();
   }
 }
